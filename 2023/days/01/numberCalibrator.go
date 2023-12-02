c73beaf293f668652ce165dca770e78f43d20e70
Miller Meares <mmeares@amazon.com>
2023-12-02 00:22:58 -0500

    Further improve readability by extracting different problems into their own files

---
+++ 2023/days/01/numberCalibrator.go
@@ -0,0 +1,35 @@
+package days
+
+import "unicode"
+
+func getCalibrationValue(input string) int {
+	base := int(firstDigit(input)) * 10
+	ones := int(lastDigit(input))
+	return base + ones
+}
+
+// On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+func firstDigit(input string) rune {
+	for _, char := range input {
+		if !isDigit(char) {
+			continue
+		}
+		return char - '0'
+	}
+	panic("Expected to find an integer")
+}
+
+func lastDigit(input string) rune {
+	for i := len(input) - 1; i >= 0; i-- {
+		char := rune(input[i])
+		if !isDigit(char) {
+			continue
+		}
+		return char - '0'
+	}
+	panic("Expected to find an integer")
+}
+
+func isDigit(input rune) bool {
+	return unicode.IsDigit(input)
+}
--- 2023/days/01/solve.go
+++ 2023/days/01/solve.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode"
 )
 
 func SolveConsideringOnlyNumbers() {
@@ -31,38 +30,6 @@ func solve(calibrator func(input string) int) {
 	}
 
 	fmt.Println("Calibration value: ", total)
-}
-
-func getCalibrationValue(input string) int {
-	base := int(firstDigit(input)) * 10
-	ones := int(lastDigit(input))
-	return base + ones
-}
-
-// On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
-func firstDigit(input string) rune {
-	for _, char := range input {
-		if !isDigit(char) {
-			continue
-		}
-		return char - '0'
-	}
-	panic("Expected to find an integer")
-}
-
-func lastDigit(input string) rune {
-	for i := len(input) - 1; i >= 0; i-- {
-		char := rune(input[i])
-		if !isDigit(char) {
-			continue
-		}
-		return char - '0'
-	}
-	panic("Expected to find an integer")
-}
-
-func isDigit(input rune) bool {
-	return unicode.IsDigit(input)
 }
 
 func readInputIntoList() ([]string, error) {
