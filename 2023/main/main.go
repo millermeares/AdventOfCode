8f211dec51bb6d217ef5bc5cf834fb64be81da87
Miller Meares <mmeares@amazon.com>
2023-12-02 12:39:14 -0500

    completed part 1 of day 2

--- 2023/main/main.go
+++ 2023/main/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	days "days/01"
+	"bufio"
+	days "days/02"
+	"fmt"
+	"os"
 )
 
 func main() {
-	days.SolveConsideringLetters()
+	input, err := readInputIntoList("02")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	days.Solve(input)
+}
+
+func readInputIntoList(day string) ([]string, error) {
+	file, err := os.Open("../days/" + day + "/input.txt")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	return lines, nil
 }
