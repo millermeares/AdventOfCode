6bed666f674a5a3feeeb778e4a7a40055b12f003
Miller Meares <mmeares@amazon.com>
2024-12-10 11:24:51 -0500

    2024 day 10 part 2 (used same code)

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -6,14 +6,13 @@ with open("2024/10/input.txt") as file:
       l.append(int(c))
     grid.append(l)
 
-# i am currently counting routes
-def count_peaks(grid, i, j):
+def count_trails(grid, i, j):
   cur = grid[i][j]
   print(f"Counting trails at {i}, {j} with {cur} val")
   if cur == 9:
-    return [(i, j)]
+    return 1
   
-  peaks = []
+  count = 0
   changes = [(0, 1), (0, -1), (1, 0), (-1, 0)]
   for (ci, cj) in changes:
     new_i = i + ci
@@ -22,8 +21,8 @@ def count_peaks(grid, i, j):
       print(f"{new_i}, {new_j} would take off of grid")
       continue # this would take off of grid
     if grid[new_i][new_j] == cur + 1:
-      peaks += count_peaks(grid, new_i, new_j)
-  return peaks
+      count += count_trails(grid, new_i, new_j)
+  return count
 
 
 def part1(grid):
@@ -31,7 +30,7 @@ def part1(grid):
   for i in range(0, len(grid)):
     for j in range(0, len(grid[i])):
       if grid[i][j] == 0:
-        total += len(set(count_peaks(grid, i, j)))
+        total += count_trails(grid, i, j)
   return total
 
 print(part1(grid))
