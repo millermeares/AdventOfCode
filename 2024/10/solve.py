225c32f938051de3076b13c1918e8c342c74ec69
Miller Meares <mmeares@amazon.com>
2024-12-10 12:40:04 -0500

    experimenting with yield function

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -7,18 +7,16 @@ trailheads = [
 def paths_to_peaks(grid, i, j):
   cur = grid[i][j]
   if cur == 9:
-    return [(i, j)]
+    yield (i, j)
   
-  peaks = []
   for (ci, cj) in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
     new_i = i + ci
     new_j = j + cj
     if new_i < 0 or new_j < 0 or new_i >= len(grid) or new_j >= len(grid[i]):
       continue # this would take off of grid
     if grid[new_i][new_j] == cur + 1:
-      peaks += paths_to_peaks(grid, new_i, new_j)
-  return peaks
+      yield from paths_to_peaks(grid, new_i, new_j)
 
 
 print(sum(len(set(paths_to_peaks(grid, i, j))) for (i, j) in trailheads))
-print(sum(len(paths_to_peaks(grid, i, j)) for (i, j) in trailheads))
+print(sum(len(list(paths_to_peaks(grid, i, j))) for (i, j) in trailheads))
