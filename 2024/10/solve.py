63d1e91525634ec13cc8c00c745e593d0e7606fc
Miller Meares <mmeares@amazon.com>
2024-12-10 11:27:35 -0500

    2024 day 10, 2 distinct solutions for each part

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -1,5 +1,5 @@
 grid = []
-with open("2024/10/input.txt") as file:
+with open("2024/10/sample.txt") as file:
   for line in file:
     l = []
     for c in line.rstrip():
@@ -8,7 +8,6 @@ with open("2024/10/input.txt") as file:
 
 def count_trails(grid, i, j):
   cur = grid[i][j]
-  print(f"Counting trails at {i}, {j} with {cur} val")
   if cur == 9:
     return 1
   
@@ -18,14 +17,37 @@ def count_trails(grid, i, j):
     new_i = i + ci
     new_j = j + cj
     if new_i < 0 or new_j < 0 or new_i >= len(grid) or new_j >= len(grid[i]):
-      print(f"{new_i}, {new_j} would take off of grid")
       continue # this would take off of grid
     if grid[new_i][new_j] == cur + 1:
       count += count_trails(grid, new_i, new_j)
   return count
 
+def count_peaks(grid, i, j):
+  cur = grid[i][j]
+  if cur == 9:
+    return [(i, j)]
+  
+  peaks = []
+  changes = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+  for (ci, cj) in changes:
+    new_i = i + ci
+    new_j = j + cj
+    if new_i < 0 or new_j < 0 or new_i >= len(grid) or new_j >= len(grid[i]):
+      continue # this would take off of grid
+    if grid[new_i][new_j] == cur + 1:
+      peaks += count_peaks(grid, new_i, new_j)
+  return peaks
+
 
 def part1(grid):
+  total = 0
+  for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+      if grid[i][j] == 0:
+        total += len(set(count_peaks(grid, i, j)))
+  return total
+
+def part2(grid):
   total = 0
   for i in range(0, len(grid)):
     for j in range(0, len(grid[i])):
@@ -34,3 +56,4 @@ def part1(grid):
   return total
 
 print(part1(grid))
+print(part2(grid))
