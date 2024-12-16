b4085cff0d51a07984fc6865b9a2a3aedb394f3d
Miller Meares <mmeares@amazon.com>
2024-12-16 15:21:49 -0500

    2024 day 16 part 1

--- 2024/16/solve.py
+++ 2024/16/solve.py
@@ -5,15 +5,12 @@ import random
 sys.setrecursionlimit(10**6)
 
 maze = []
-with open("2024/16/sample2.txt") as file:
+with open("2024/16/input.txt") as file:
   for line in file:
     maze.append(list(line.rstrip()))
 
-
-def cheapest_path(maze, x, y, prevX, prevY, visited, memo):
-  mk = memo_key(x, y, prevX, prevY, visited)
-  if mk in memo:
-    return memo[mk]
+# this works but is too expensive.
+def cheapest_path(maze, x, y, prevX, prevY, visited):
   print(f"Path from ({x}, {y})")
   if maze[y][x] == 'E':
     return 0 # we're here! 
@@ -27,19 +24,18 @@ def cheapest_path(maze, x, y, prevX, prevY, visited, memo):
       continue # already visited
     # no need to account for backwards moves because 'visited' does that.
     move_cost = 1001 if is_90_turn(x, y, prevX, prevY, nx, ny) else 1
-    cost = min(cost, move_cost + cheapest_path(maze, nx, ny, x, y, visited, memo))
+    cost = min(cost, move_cost + cheapest_path(maze, nx, ny, x, y, visited))
   visited.remove((x, y))
-  memo[mk] = cost
   return cost
 
 def is_90_turn(x, y, prevX, prevY, nx, ny):
   return not ((x == prevX and prevX == nx) or (y == prevY and prevY == ny))
   
 
-def find_start(maze):
+def find_location(maze, location):
   for y, line in enumerate(maze):
     for x, c in enumerate(line):
-      if c == 'S': return (x, y)
+      if c == location: return (x, y)
   raise Exception("could not find start")
 
 def print_maze(maze, visited):
@@ -52,18 +48,67 @@ def print_maze(maze, visited):
         l += c
     print(l)
 
-def memo_key(x, y, prevX, prevY, visited):
-  # l_v = list(visited)
-  # l_v.sort()
-  # v_str = ''
-  # for (vx, vy) in l_v:
-  #   v_str += f"({vx},{vy})"
-  return f"({x},{y}),({prevX},{prevY})" #-{v_str}"
+def get_cost_grid(maze):
+  grid = []
+  for line in maze:
+    gl = []
+    for c in line:
+      # it's *possible* that i need to add 4 directions rather than 2.
+      gl.append({
+        'vertical': sys.maxsize,
+        'horizontal': sys.maxsize
+      })
+    grid.append(gl)
+  return grid
+
+
+def fill(maze, costs):
+  queue = []
+  (ex, ey) = find_location(maze, 'E')
+  costs[ey][ex]['vertical'] = 0
+  costs[ey][ex]['horizontal'] = 0
+  queue.append((ex, ey))
+  while len(queue) > 0:
+    (x, y) = queue.pop(0)
+    print(x, y)
+    neighbors = get_neighbors(maze, x, y)
+    for (nx, ny) in neighbors:
+      should_update = False
+      is_horizontal = ny == y
+      horiz_add = 1 if is_horizontal else 1001
+      vert_add = 1001 if is_horizontal else 1
+      cost_to_neighbor = costs[y][x]['horizontal'] if is_horizontal else costs[y][x]['vertical']
+      horiz_cost = cost_to_neighbor + horiz_add
+      if costs[ny][nx]['horizontal'] > horiz_cost:
+        costs[ny][nx]['horizontal'] = horiz_cost
+        should_update = True
+      vert_cost = cost_to_neighbor + vert_add
+      if costs[ny][nx]['vertical'] > vert_cost:
+        costs[ny][nx]['vertical'] = vert_cost
+        should_update = True
+      if should_update:
+        queue.append((nx, ny))
+
+  
+
+def get_neighbors(maze, x, y):
+  neighbors = []
+  changes = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+  for (cx, cy) in changes:
+    (nx, ny) = (x+cx,y+cy)
+    if maze[ny][nx] != '#':
+      neighbors.append((nx, ny))
+  return neighbors
+
 
 
 def part1(maze):
-  (sx, sy) = find_start(maze)
-  return cheapest_path(maze, sx, sy, sx-1, sy, set(), {})
+  costs = get_cost_grid(maze)
+  # can i do with dp?
+  (sx, sy) = find_location(maze, 'S')
+  fill(maze, costs)
+  return costs[sy][sx]
+  # return cheapest_path(maze, sx, sy, sx-1, sy, set())
 
 
 
