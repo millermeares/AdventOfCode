a8b0b7933129f6d033971ea3323faafc1a85b11b
Miller <mmeares@amazon.com>
2025-12-03 11:35:43 -0500

    2025 day 3 part 2 with DFS recursion & memoization

--- 2025/aoc/src/days/day03/mod.rs
+++ 2025/aoc/src/days/day03/mod.rs
@@ -1,4 +1,6 @@
-use crate::Day;
+use std::collections::HashMap;
+
+use crate::{Day, timed};
 
 pub struct Day03 {}
 
@@ -7,20 +9,32 @@ struct Bank {
 }
 
 impl Bank {
-    fn max_joltage(&self) -> i64 {
+    fn max_joltage(&self, start_idx: usize, nums_to_choose: usize, memo: &mut HashMap<String, i64>) -> i64 {
+        if nums_to_choose == 0 {
+            panic!("cannot choose zero numbers");
+        }
+        let memo_key = format!("{start_idx}-{nums_to_choose}").to_string();
+        if memo.contains_key(&memo_key) {
+            return memo[&memo_key]
+        }
+
         // find two largest numbers in my batteries array. 
-        let mut max_num: i32 = 0;
-        for l in 0..self.batteries.len()-1 {
-            for r in l+1..self.batteries.len() {
-                let lnum = self.batteries.get(l).unwrap();
-                let rnum = self.batteries.get(r).unwrap();
-                let num = format!("{lnum}{rnum}").parse::<i32>().unwrap();
-                if num > max_num {
-                    max_num = num;
-                }
+        let mut max_num = 0;
+        for i in start_idx..self.batteries.len()-nums_to_choose+1 {
+            let max_rest_if_chosen: String = if nums_to_choose > 1 {
+                self.max_joltage(i+1, nums_to_choose-1, memo).to_string()
+            } else {
+                "".to_string()
+            };
+            let num= self.batteries.get(i).unwrap();
+            let num_if_chosen = format!("{num}{max_rest_if_chosen}").parse::<i64>().unwrap();
+            if num_if_chosen > max_num {
+                max_num = num_if_chosen
             }
         }
-        max_num as i64
+        let v = max_num as i64;
+        memo.insert(memo_key.to_string(), v.clone());
+        memo[&memo_key]
     }
 }
 
@@ -28,13 +42,15 @@ impl Day for Day03 {
     fn solve_1(&self, input: String) -> i64 {
         let banks = parse_banks(input);
         let mut max_joltage = 0;
-        banks.iter().for_each(|b| max_joltage += b.max_joltage());
+        banks.iter().for_each(|b| max_joltage += b.max_joltage(0, 2, &mut HashMap::new()));
         max_joltage
     }
 
     fn solve_2(&self, input: String) -> i64 {
-        // needs to be some search algorithm which is like 'take biggest' starting with this. DFS basically i think with recursion.
-        todo!()
+        let banks = parse_banks(input);
+        let mut max_joltage = 0;
+        banks.iter().for_each(|b| max_joltage += timed!("Max joltage calculation", b.max_joltage(0, 12, &mut HashMap::new())));
+        max_joltage
     }
 }
 
@@ -62,5 +78,8 @@ mod tests {
     fn example_2() {
         let d = Day03{};
         assert_eq!(987654321111, d.solve_2("987654321111111".to_string()));
+        assert_eq!(811111111119, d.solve_2("811111111111119".to_string()));
+        assert_eq!(434234234278, d.solve_2("234234234234278".to_string()));
+        assert_eq!(888911112111, d.solve_2("818181911112111".to_string()));
     }
 }
